19030fd7544b3988f5f6728b8fffa969d42fd2ca
Ao Mo-Hellenbrand <hellenbr@in.tum.de>
2017-10-24 16:19:36 +0200

    initial add

---
+++ src/maxrank.cpp
@@ -0,0 +1,80 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/** iMPI related from <mpi.h>
+
+#define MPI_ADAPT_FALSE             0
+#define MPI_ADAPT_TRUE              1
+#define MPI_ADAPT_STATUS_NEW        2
+#define MPI_ADAPT_STATUS_JOINING    3
+#define MPI_ADAPT_STATUS_STAYING    4
+#define MPI_ADAPT_STATUS_LEAVING 	5
+int MPI_Init_adapt(int *argc, char ***argv, int *local_status);
+int MPI_Probe_adapt(int *current_operation, int *local_status, MPI_Info *info);
+int MPI_Comm_adapt_begin(MPI_Comm *intercomm, MPI_Comm *new_comm_world, int *staying_count, int *leaving_count, int *joining_count);
+int MPI_Comm_adapt_commit(void);
+
+*/
+
+
+void impi_adapt(
+		int& mpisize,
+		int& mpirank,
+		int& mpistatus,
+		int& iter) 
+{
+	int adapt_flag;
+	MPI_Info mpi_info;
+	MPI_Comm intercomm, newcomm;
+	int numstay, numleave, numjoin;
+
+	MPI_Probe_adapt(&adapt_flag, &mpistatus, &mpi_info);
+	if (adapt_flag == MPI_ADAPT_TRUE) {
+		MPI_Comm_adapt_begin(&intercomm, &newcomm, &numstay, &numleave, &numjoin);
+		//************* adapt window ***************
+		printf("Adapting... %d staying, %d leaving, %d joining.", numstay, numleave, numjoin);
+		MPI_Bcast(&iter, 1, MPI_INT, 0, newcomm);
+		//*********** adapt window end *************
+		MPI_Comm_adapt_commit();
+		MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+		MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+	}
+	return;
+}
+
+
+int main(int argc, char* argv[]) {
+
+	int mpisize, mpirank, mpistatus;
+
+	MPI_Init_adapt(&argc, &argv, &mpistatus);
+	MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+	MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+
+	int iter = 0, itermax = 10;
+
+	// Joining ranks adapt directly
+	if (mpistatus == MPI_ADAPT_STATUS_JOINING) {
+		impi_adapt(mpisize, mpirank, mpistatus, iter);
+	}
+
+	// Main loop
+	while (iter < itermax) {
+		iter++;
+
+		if (mpirank == 0) {
+			printf("Iteration %d:", iter);
+			printf("Total ranks: %d. First rank: %d.", mpisize, mpirank);
+		}
+		if (mpirank == mpisize-1) {
+			printf("Total ranks: %d. Last rank: %d.\n\n", mpisize, mpirank);
+		}
+
+		sleep(120);
+		impi_adapt(); //impi_adapt must be at END of loop
+	}
+	MPI_Finalize();
+	return 0;
+}
+
